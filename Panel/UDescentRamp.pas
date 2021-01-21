30483a6a328abb409673b25f85693e0f730ba06b
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2021-01-20 23:00:46 -0300

    draw grid on descent ramp

--- Panel/UDescentRamp.pas
+++ Panel/UDescentRamp.pas
@@ -10,6 +10,7 @@ type
     RunwayAltFt, RunwayDistNm, AirplaneAltFt: Extended;
 
     function GetPos(AltFt, DistNm: Extended): TPointF;
+    procedure PaintGrid;
   protected
     procedure Paint; override;
   public
@@ -41,18 +42,43 @@ var
   StartPos, PlanePos: TPointF;
 begin
   inherited;
+  Canvas.Stroke.Kind := TBrushKind.Solid;
+
+  PaintGrid;
   if (RunwayDistNm = 0) or (RunwayDistNm > WIDTH_NM) then Exit;
 
   StartPos := GetPos(IDEAL_START_ALT_FT, IDEAL_START_DIST_NM);
   PlanePos := GetPos(AirplaneAltFt - RunwayAltFt, RunwayDistNm);
 
-  Canvas.Stroke.Kind := TBrushKind.Solid;
+  Canvas.Stroke.Thickness := 1;
 
   Canvas.Stroke.Color := TAlphaColors.Green;
   Canvas.DrawLine(StartPos, LocalRect.BottomRight, 1);
 
   Canvas.Stroke.Color := TAlphaColors.Red;
   Canvas.DrawLine(PlanePos, LocalRect.BottomRight, 1);
+end;
+
+procedure TDescentRamp.PaintGrid;
+var
+  AltFt, DistNm: Integer;
+begin
+  Canvas.Stroke.Color := TAlphaColors.Gray;
+  Canvas.Stroke.Thickness := 0.2;
+
+  AltFt := 0;
+  while AltFt<HEIGHT_FT do
+  begin
+    Inc(AltFt, 1000);
+    Canvas.DrawLine(GetPos(AltFt, WIDTH_NM), GetPos(AltFt, 0), 1);
+  end;
+
+  DistNm := 0;
+  while DistNm<WIDTH_NM do
+  begin
+    Inc(DistNm, 1);
+    Canvas.DrawLine(GetPos(HEIGHT_FT, DistNm), GetPos(0, DistNm), 1);
+  end;
 end;
 
 procedure TDescentRamp.UpdateData(L: TPropertyList);
